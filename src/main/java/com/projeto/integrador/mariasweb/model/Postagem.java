f4357bb80e0b7d8346d80f9ecc5bf56b76ed24a9
nathaliacrz <nathalias161@gmail.com>
2022-07-07 08:49:56 -0300

    atualizando tabela postagem

--- src/main/java/com/projeto/integrador/mariasweb/model/Postagem.java
+++ src/main/java/com/projeto/integrador/mariasweb/model/Postagem.java
@@ -7,12 +7,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 
@@ -34,6 +37,18 @@ public class Postagem {
 	
 	@UpdateTimestamp
 	private LocalDateTime date;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 	public Long getId() {
 		return id;
